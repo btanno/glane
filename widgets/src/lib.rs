b65dd93dde7b29d524e799f736a89db501d2f8fb
btanno <149578422+btanno@users.noreply.github.com>
2024-08-11 13:34:22 +0900

    Emptyを追加

--- widgets/src/lib.rs
+++ widgets/src/lib.rs
@@ -1,19 +1,21 @@
 pub mod button;
-pub mod label;
 pub mod container;
-pub mod text_box;
-pub mod scroll_bar;
-pub mod list_box;
-pub mod text;
 pub mod dropdown_box;
+pub mod empty;
+pub mod label;
+pub mod list_box;
+pub mod scroll_bar;
+pub mod text;
+pub mod text_box;
 
 pub use button::Button;
-pub use label::Label;
 pub use container::*;
-pub use text_box::TextBox;
-pub use scroll_bar::ScrollBar;
-pub use list_box::ListBox;
-pub use text::Text;
 pub use dropdown_box::DropdownBox;
+pub use empty::Empty;
+pub use label::Label;
+pub use list_box::ListBox;
+pub use scroll_bar::ScrollBar;
+pub use text::Text;
+pub use text_box::TextBox;
 
 use glane_core::*;
